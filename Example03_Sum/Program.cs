db51bd6d6bf82d09d4fd715f03751037739e5e2b
Dimishack <dimishack.git@mail.ru>
2023-06-21 14:58:40 +0300

    Replace ReadLine Random

--- Example03_Sum/Program.cs
+++ Example03_Sum/Program.cs
@@ -1,3 +1,3 @@
-﻿int Value1 = Int32.Parse(Console.ReadLine());
-int Value2 = Int32.Parse(Console.ReadLine());
-Console.WriteLine(Value1 + Value2);
+﻿int Value1 = new Random().Next(1, 10);
+int Value2 = new Random().Next(1, 10);
+Console.WriteLine("{0} + {1} = {2}", Value1, Value2, Value1 + Value2);
