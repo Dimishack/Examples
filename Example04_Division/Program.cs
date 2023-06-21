12b8e0696e974b67b5aa7e90dec04fa2ed58da25
Dimishack <dimishack.git@mail.ru>
2023-06-21 14:53:25 +0300

    Create program division

---
+++ Example04_Division/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double Value1 = double.Parse(Console.ReadLine());
+        double Value2 = double.Parse(Console.ReadLine());
+        Console.WriteLine(Value1 / Value2);
+    }
+}
