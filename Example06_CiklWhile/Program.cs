56c24a3e34dcca721b249a6147bfea83f50181a2
Dimishack <dimishack.git@mail.ru>
2023-06-21 16:29:29 +0300

    Create program with cikl While

---
+++ Example06_CiklWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿int xa = 1, xb = 1, xc = 80;
+int ya = 1, yb = 30, yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine('+');
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine('+');
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine('+');
+int x = xa, y = yc;
+for(int i = 0; i<15000; i++){
+    int want = new Random().Next(0,3);
+    if (want == 0){
+        x = (x+xa) / 2;
+        y = (y+ya) / 2;
+    }
+    if (want == 1){
+        x = (x+xb) / 2;
+        y = (y+yb) / 2;
+    }
+    if (want == 2){
+        x = (x+xc) / 2;
+        y = (y+yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine('+');
+}
+Console.SetCursorPosition(1, yc+1);
